67a5d462617dae61a01b7d1a55ad87603283d638
Paramveergit <iveersoni@gmail.com>
2026-07-25 00:13:14 +0530

    Fix my own bug: productData was the raw QueryDocumentSnapshot, not its extracted .data() map - fromMap needs the Map, not the snapshot object

--- e_comm/lib/widgets/flash-sale-widget.dart
+++ e_comm/lib/widgets/flash-sale-widget.dart
@@ -65,7 +65,7 @@ class FlashSaleWidget extends StatelessWidget {
   }
 
   Widget _buildFlashSaleItem(BuildContext context, int index, List<QueryDocumentSnapshot> docs) {
-    final productData = docs[index];
+    final productData = docs[index].data() as Map<String, dynamic>;
     ProductModel productModel = ProductModel.fromMap(productData);
     
     return Row(
